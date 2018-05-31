702e4aac924025431b2c6acbfcea683a252cc9a6
Jossie <jossiemathews@gmail.com>
2018-05-31 16:02:55 +0530

    all servers are added to the already built html

--- js/main.js
+++ js/main.js
@@ -1,116 +1,52 @@
 $(function() {
     
-    var jsonData = {
-        rack_size: 40,
-        hosts: [
-            {
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 1,
-            },
-            {
-                ip: '10.02.102',
-                name: "cdn.allprobe",
-                type: "cdn",
-                status: "okay",
-                u: 1
-            },
-            {
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 3
-            },
-
-            {
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 4
-            },
-
-            {
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 5
-            },
-
-            /*{
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 5
-            },
-
-            {
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 5
-            },
-            {
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 5
-            },
-            {
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 3
-            },
-            {
-                ip: '10.02.100',
-                name: "allprobe",
-                type: "san",
-                status: "okay",
-                u: 3
-            },*/
-            
+    var jsonData = 
+    {
+        "name":"M-02 \/ 1x",
+        "size":"42",
+        "hosts":[
+            {"name":"SRV18","size":1,"bucket":"Virtual Platforms Grid-2","ip":"62.90.132.135","events":""},
+            {"name":"DEBIAN-FREE-3TB","size":1,"bucket":"ADCD core","ip":"62.90.132.132","events":""},
+            {"name":"SRV20","size":1,"bucket":"Virtual Platforms Grid-2","ip":"62.90.132.151","events":""},
+            {"name":"LAN - SWITCH","size":1,"bucket":"ADCD core","ip":"127.0.0.1","events":""},
+            {"name":"GW-1","size":1,"bucket":"ADCD core","ip":"62.90.132.12","events":""},
+            {"name":"WAN - SWITCH","size":1,"bucket":"ADCD core","ip":"127.0.0.1","events":""},
+            {"name":"NFS storage","size":1,"bucket":"ADCD core","ip":"192.168.0.70","events":""},
+            {"name":"APR-CAS1","size":1,"bucket":"AllProbe Cassandra","ip":"192.168.0.181","events":""},
+            {"name":"SRV27","size":1,"bucket":"Virtual Platforms Grid-2","ip":"62.90.132.23","events":""},
+            {"name":"GW-2","size":1,"bucket":"ADCD core","ip":"62.90.132.21","events":""},
+            {"name":"HA-PROXY1","size":1,"bucket":"ADCD core","ip":"62.90.132.15","events":""},
+            {"name":"APR-CAS2","size":1,"bucket":"AllProbe Cassandra","ip":"192.168.0.182","events":""},
+            {"name":"SRV28","size":4,"bucket":"Virtual Platforms Grid-2","ip":"62.90.132.10","events":""},
+            {"name":"APR-CAS3","size":1,"bucket":"AllProbe Cassandra","ip":"192.168.0.183","events":""},
+            {"name":"SRV23","size":1,"bucket":"Virtual Platform - SELF (grid2)","ip":"62.90.132.174","events":""},
+            {"name":"SRV29","size":6,"bucket":"Virtual Platforms Grid-2","ip":"62.90.132.140","events":""}
         ]
     };
-     
+
+    var unitHeight = 20;
     var height = 0;
+    var div;
+    
     $.each(jsonData.hosts, function(index, value) {
         
-        console.log(value);
-        var div =
-        $(".rack_body").append(
-            $("<div><div class='individual-server-first'></div><div class='individual-server-second'></div><div class='individual-server-third'></div></div>").addClass("individual-server")
-            .css("height", (value.u * 13) + "px").append(
-                $("<div></div>").addClass("switch")
-                
+        div =
+        $(".rack_body")
+        .append(
+            $( "<div>" + 
+                    "<div class='individual-server-first'>" + (index + 1) + "</div>" +
+                    "<div class='individual-server-second'>" + 
+                        "<span>"+ value.name + "</span>" + 
+                    "</div>" +
+                    "<div class='individual-server-third'></div>" + 
+                "</div>" 
             )
+            .addClass("individual-server")
+            .attr("index", index)
+            .css("height", (value.size * unitHeight) + "px")
         );
-
-        height = height + (value.u * 13);
                     
     });
-
-    var times = Math.round((height - 60) / 13);
-    height = height + jsonData.hosts.length;
-
-    //$(".rack_body").css("height", height + "px");
-
-    
-    console.log(times);
-    var single_rack = $(".single_rack").clone();
-    for(var i = 0; i < times; i++) {
-        //$('.rack_body').append(single_rack);
-        //single_rack = single_rack.clone();
-        console.log(i);
-    }
 
 });
 
