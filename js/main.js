7455f8bdc39c286684995443ea72b575ce4cf2d7
Jossie <jossiemathews@gmail.com>
2018-06-16 16:25:21 +0530

    gree/red and ajax implememntation

--- js/main.js
+++ js/main.js
@@ -2,28 +2,14 @@ $(function() {
     // Idea.
     // From the main data, split the data to two parts; 1) All the data with pos 0 and 2) All the data with pos != 0 AND SORT
     // Then create a new array with size 42, arrange the data just like the rack.
-
-    var jsonData = 
-    {
-        "name":"M-02 \/ 1x",
-        "size":"42",
-        "hosts":[
-            {"name":"SRV18","size":1,"bucket":"Virtual Platforms Grid-1","ip":"62.90.132.135","events":"","submenu":"ONCLICK SUBMENU CONTENT","pos":"20"},
-            {"name":"SRV20","size":5,"bucket":"Virtual Platforms Grid-1","ip":"62.90.132.151","events":"","submenu":"ONCLICK SUBMENU CONTENT","pos":"0"},
-            {"name":"SRV27","size":1,"bucket":"Virtual Platforms Grid-1","ip":"62.90.132.23","events":"","submenu":"ONCLICK SUBMENU CONTENT","pos":"14"},
-            {"name":"SRV28","size":4,"bucket":"Virtual Platforms Grid-1","ip":"62.90.132.10","events":"","submenu":"ONCLICK SUBMENU CONTENT","pos":"10"},
-            {"name":"SRV23","size":1,"bucket":"Virtual Platforms Grid-1","ip":"62.90.132.174","events":"","submenu":"ONCLICK SUBMENU CONTENT","pos":"1"},
-            {"name":"SRV29","size":1,"bucket":"Virtual Platforms Grid-1","ip":"62.90.132.140","events":"","submenu":"ONCLICK SUBMENU CONTENT","pos":"0"}
-        ]
-    };
-
+    var jsonData;
     var unitHeight = 20;
     var maxLength = 42;
     var reverseL = 43;
     var height = 0;
     var div;
     var running = 1;
-    var hosts = jsonData.hosts;
+    var hosts;
     var dummy = {"name": "", size: 1};
     var rackBodyPos = $('.rack_container').position();
     
@@ -31,126 +17,169 @@ $(function() {
     nonZeroPosHosts = [];
     wholeHosts = [];
     indexArray = [];
-    
+
+    $.get('data.json', function(data) {
+        console.log("Hola", data);
+        jsonData = data;    
+        initialize();
+        divideData();
+        sortData();
+        combineDividedArrays();
+        render();
+    });
+ 
     $('.sub_menu_rectangle').mouseout(function() {
         $(this).hide();
     });
 
-    $.each(hosts, function(index, host) {
-        host.pos = parseInt(host.pos);
-        if(host.pos === 0) {
-            zeroPosHosts.push(host);
-        } else if( host.pos > 0 ) {
-            nonZeroPosHosts.push(host);
-        }
-    });
-
-    nonZeroPosHosts.sort(function(a, b) {
-        return a.pos - b.pos;
-    });
-
-    hosts.sort(function(a, b) {
-        return a.pos - b.pos;
-    });
-
-    var i = 1;
-    var dummyData = {"name":"",'dummy': true, "size":1,"bucket":"","ip":"","events":"","submenu":"","pos": null};
+    initialize = function () {
+        hosts = jsonData.hosts;
+    };
     
-    while(i <= 42) {
-        if(nonZeroPosHosts[0]) {
-            if(nonZeroPosHosts[0].pos === i) {
-                wholeHosts[i - 1] = $.extend({}, nonZeroPosHosts[0]);
-                i = i + nonZeroPosHosts[0].size;
-                nonZeroPosHosts.shift();
+    divideData = function() {
+
+        $.each(hosts, function(index, host) {
+            host.pos = parseInt(host.pos);
+            if(host.pos === 0) {
+                zeroPosHosts.push(host);
+            } else if( host.pos > 0 ) {
+                nonZeroPosHosts.push(host);
+            }
+        });    
+    };
+    
+    sortData = function() {
+        nonZeroPosHosts.sort(function(a, b) {
+            return a.pos - b.pos;
+        });
+    
+        hosts.sort(function(a, b) {
+            return a.pos - b.pos;
+        });
+    };
+    
+    combineDividedArrays = function() {
+        var i = 1;
+        var dummyData = {
+            "name":"",
+            'dummy': true, 
+            "size":1,
+            "bucket":"",
+            "ip":"",
+            "events":"",
+            "submenu":"",
+            "pos": null
+        };
+        
+        while(i <= 42) {
+            if(nonZeroPosHosts[0]) {
+                if(nonZeroPosHosts[0].pos === i) {
+                    wholeHosts[i - 1] = $.extend({}, nonZeroPosHosts[0]);
+                    i = i + nonZeroPosHosts[0].size;
+                    nonZeroPosHosts.shift();
+                    continue;
+                }
+            } 
+
+            if(zeroPosHosts[0]) {
+                wholeHosts[i - 1] = $.extend({}, zeroPosHosts[0]);
+                i = i + zeroPosHosts[0].size;
+                zeroPosHosts.shift();
                 continue;
             }
-        } 
 
-        if(zeroPosHosts[0]) {
-            wholeHosts[i - 1] = $.extend({}, zeroPosHosts[0]);
-            i = i + zeroPosHosts[0].size;
-            zeroPosHosts.shift();
-            continue;
+            wholeHosts[i - 1] = dummyData;
+            i = i + 1;
         }
+    };
 
-        // 
+    render = function() {
 
-        wholeHosts[i - 1] = dummyData;
-
-        i = i + 1;
-    }
-
-    console.log("All data", wholeHosts);
-
-    $.each(wholeHosts, function(index, value) {
+        $.each(wholeHosts, function(index, value) {
         
-        if(value) {
-
-            var hString = "";
-
-            for(var i = 0; i < value.size; i++) {
-                
-                if(i === 0 ) {
-                    indexArray[i] = "<div class='individual-server-first-child' style='border-bottom:none'>" + ( running ) + "</div>";
-                } else {
-                    indexArray[i] = "<div class='individual-server-first-child'>" + ( running ) + "</div>";
-                }
-                
-                running = running + 1; 
-            }
-
-            hString = indexArray.reverse().join('');
-            indexArray.length = 0;
-            
-            div = $( "<div>" + 
-                        "<div class='individual-server-first'>" + hString + "</div>" +
-                        "<div class='individual-server-second'>" + 
-                            "<span>"+ value.name + "</span>" + 
-                        "</div>" +
-                        "<div class='individual-server-third'></div>" + 
-                        "</div>" 
-                    )
-                    .addClass("individual-server")
-                    .attr("index", index)
-                    .css("height", (value.size * unitHeight) + "px");
-
-            $(".rack_body")
-                .prepend(div);
-            
-            
-            if(! value.dummy) {
-                
-                $(div).data("myJson", value);
-                
-                $(div).click(function(event) {
-                    $('.data_rectangle').hide();
-                    var jsonContent = $(this).data('myJson');
-                    console.log($(this).data('myJson'), event.pageX, event.pageY);
-                    var newPos = { top: event.pageY - 10, left: event.pageX - 10 };
-                    $('.sub_menu_rectangle').html(jsonContent.submenu)
-                        .css(newPos)
-                        .show();
-                });
+            if(value) {
     
-                $(div).mousemove(function(event) {
-                    var jsonContent = $(this).data('myJson');
-                    console.log("sdfdsfs")
-                    var divPos = $(this).position();
-                    var newPos = { top: event.pageY, left: event.pageX + 10 };
-                    $('.data_rectangle').text(jsonContent.ip)
-                        .css(newPos)
-                        .show();
+                var hString = "";
+    
+                for(var i = 0; i < value.size; i++) {
+                    var colorText = "";
+                    if(! value.dummy) {
+                        if(value.events === "") {
+                            colorText = "background-color: green";
+                        } else if(value.events.length > 0) {
+                            colorText = "background-color: red";
+                        }
+                    }
+                    if(i === 0 ) {
+                        indexArray[i] = "<div class='individual-server-first-child' style='border-bottom:none;" + colorText + "'>" + ( running ) + "</div>";
+                    } else {
+                        indexArray[i] = "<div class='individual-server-first-child' style='"+ colorText +"'>" + ( running ) + "</div>";
+                    }
                     
-                });
-
-                $(div).mouseout(function() {
-                    $('.data_rectangle').hide();
-                });
-            }
-        }
-        
-        console.log(value, index);
+                    running = running + 1; 
+                }
+    
+                hString = indexArray.reverse().join('');
+                indexArray.length = 0;
+                
+                div = $( "<div>" + 
+                            "<div class='individual-server-first'>" + hString + "</div>" +
+                            "<div class='individual-server-second'>" + 
+                                "<span style='cursor: pointer;'>"+ value.name + "</span>" + 
+                            "</div>" +
+                            "<div class='individual-server-third'></div>" + 
+                            "</div>" 
+                        )
+                        .addClass("individual-server")
+                        .attr("index", index)
+                        .css("height", (value.size * unitHeight) + "px");
+    
+                $(".rack_body")
+                    .prepend(div);
+                
+                
+                if(! value.dummy) {
                     
-    });
+                    $(div).data("myJson", value);
 
+                    implementClick(div);
+                    implementMouseMove(div);
+                    implementMouseOut(div);
+                    
+                }
+            }                    
+        });
+    
+    };
+
+    implementMouseOut = function(div) {
+        $(div).mouseout(function() {
+            $('.data_rectangle').hide();
+        });
+    };
+
+    implementMouseMove = function(div) {
+        $(div).mousemove(function(event) {
+            var jsonContent = $(this).data('myJson');
+            var divPos = $(this).position();
+            var newPos = { top: event.pageY, left: event.pageX + 10 };
+            $('.data_rectangle').text(jsonContent.ip)
+                .css(newPos)
+                .show();
+            
+        });
+    };
+
+    implementClick = function(div) {
+        $(div).click(function(event) {
+            $('.data_rectangle').hide();
+            var jsonContent = $(this).data('myJson');
+            console.log($(this).data('myJson'), event.pageX, event.pageY);
+            var newPos = { top: event.pageY - 10, left: event.pageX - 10 };
+            $('.sub_menu_rectangle').html(jsonContent.submenu)
+                .css(newPos)
+                .show();
+        });
+    };
+    
 });
